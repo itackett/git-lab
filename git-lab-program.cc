d06bb5169cb91b5967ec7b638da9b8baf50cef0f
Isaac Tackett <it395822@ohio.edu>
2023-01-25 17:47:43 -0500

    Hello git

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Isaac Tackett>
+ *        Date: <1/25/2023>
+ * Description: Lab about pushing files and github.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
